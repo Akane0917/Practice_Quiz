a267df453a818779ee89613e361b4a496a4ec81e
Akane0917 <tennis.aka.100@gmail.com>
2025-11-13 11:20:20 -0500

    akane

--- Practice_Quiz/Form1.Designer.cs
+++ Practice_Quiz/Form1.Designer.cs
@@ -78,18 +78,19 @@
             // 
             // question01Button
             // 
-            this.question01Button.Location = new System.Drawing.Point(5, 147);
+            this.question01Button.Location = new System.Drawing.Point(18, 164);
             this.question01Button.Name = "question01Button";
             this.question01Button.Size = new System.Drawing.Size(119, 35);
             this.question01Button.TabIndex = 3;
             this.question01Button.Text = "Question 01";
             this.question01Button.UseVisualStyleBackColor = true;
+            this.question01Button.Click += new System.EventHandler(this.question01Button_Click);
             // 
             // question1OutputLabel
             // 
-            this.question1OutputLabel.Location = new System.Drawing.Point(1, 185);
+            this.question1OutputLabel.Location = new System.Drawing.Point(73, 205);
             this.question1OutputLabel.Name = "question1OutputLabel";
-            this.question1OutputLabel.Size = new System.Drawing.Size(247, 47);
+            this.question1OutputLabel.Size = new System.Drawing.Size(247, 41);
             this.question1OutputLabel.TabIndex = 4;
             this.question1OutputLabel.Text = "question1OutputLabel";
             // 
@@ -118,12 +119,13 @@
             this.question2Button.TabIndex = 7;
             this.question2Button.Text = "Question 02";
             this.question2Button.UseVisualStyleBackColor = true;
+            this.question2Button.Click += new System.EventHandler(this.question2Button_Click);
             // 
             // question2TextBox
             // 
-            this.question2TextBox.Location = new System.Drawing.Point(5, 385);
+            this.question2TextBox.Location = new System.Drawing.Point(12, 388);
             this.question2TextBox.Name = "question2TextBox";
-            this.question2TextBox.Size = new System.Drawing.Size(286, 26);
+            this.question2TextBox.Size = new System.Drawing.Size(286, 31);
             this.question2TextBox.TabIndex = 8;
             // 
             // question2OutputLabel
@@ -159,19 +161,20 @@
             this.question3Button.TabIndex = 12;
             this.question3Button.Text = "Question 03";
             this.question3Button.UseVisualStyleBackColor = true;
+            this.question3Button.Click += new System.EventHandler(this.question3Button_Click);
             // 
             // q3Int1Text
             // 
             this.q3Int1Text.Location = new System.Drawing.Point(479, 281);
             this.q3Int1Text.Name = "q3Int1Text";
-            this.q3Int1Text.Size = new System.Drawing.Size(100, 26);
+            this.q3Int1Text.Size = new System.Drawing.Size(100, 31);
             this.q3Int1Text.TabIndex = 13;
             // 
             // q3int2Text
             // 
             this.q3int2Text.Location = new System.Drawing.Point(638, 281);
             this.q3int2Text.Name = "q3int2Text";
-            this.q3int2Text.Size = new System.Drawing.Size(100, 26);
+            this.q3int2Text.Size = new System.Drawing.Size(100, 31);
             this.q3int2Text.TabIndex = 14;
             // 
             // question3OutputLabel
@@ -211,7 +214,7 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 561);
             this.Controls.Add(this.label10);
@@ -237,6 +240,7 @@
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Practice Quiz";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Practice_Quiz/Form1.cs
+++ Practice_Quiz/Form1.cs
@@ -21,5 +21,43 @@ namespace Practice_Quiz
         {
 
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void question01Button_Click(object sender, EventArgs e)
+        {
+            question1OutputLabel.Text = "Happy";
+        }
+
+        private void question2Button_Click(object sender, EventArgs e)
+        {
+            string username = question2TextBox.Text;
+            question2OutputLabel.Text = "hello "+username;
+        }
+
+        private void question3Button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int num1, num2;
+                num1 = int.Parse(q3Int1Text.Text);
+                num2 = int.Parse(q3int2Text.Text);
+                double total = num1 * num2;
+
+
+                question3OutputLabel.Text = num1 + " X " + num2 + " = " + total.ToString();
+            }
+            catch
+            {
+                question3OutputLabel.Text = "please enter the number";
+            }
+            
+
+            
+
+        }
     }
 }
